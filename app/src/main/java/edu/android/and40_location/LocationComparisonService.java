dc355a7bf49b8abed656dccc6e8923b4093a9485
Taewon Eum <twon143@naver.com>
2018-10-31 17:30:41 +0900

    '산책 모드' 버튼을 눌렀을 시 백그라운드에서 실행할 서비스 클래스 생성

---
+++ app/src/main/java/edu/android/and40_location/LocationComparisonService.java
@@ -0,0 +1,55 @@
+package edu.android.and40_location;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.model.Marker;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LocationComparisonService extends IntentService {
+
+    private static final int REQ_FINE_LOCATION = 100;
+
+    public static final String TABLE_NAME = "location";
+
+    private FirebaseDatabase database;
+    private DatabaseReference locationReference;
+    private ValueEventListener valueEventListener;
+
+    // 위치 정보(최근 위치, 주기적 업데이트 시작/취소)와 관련된 클래스
+    private FusedLocationProviderClient locationClient;
+    // 주기적 위치 업데이트를 요청할 때 설정 정보를 저장하는 클래스
+    private LocationRequest locationRequest;
+    // 주기적 위치 정보를 처리하는 콜백
+    private LocationCallback locationCallback;
+
+    public LocationComparisonService() {
+        super("LocationComparisonService");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
